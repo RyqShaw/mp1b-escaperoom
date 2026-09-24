0f758749e4b23307eb8eb97cebd6b87732d32885
AReye85 <areye85@illinois.edu>
2026-09-23 20:53:12 -0500

    Red Herrings
    Added interior decorations and timothy

---
+++ Assets/Scripts/Rin/Jumpscare.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Jumpscare : MonoBehaviour
+{
+    
+
+ //   private bool jumpscare = true;
+ //   public float duration = 5f;
+ //   public Transform mainCamera;
+  //  public Vector3 camera;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+   //     camera = mainCamera.position;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
